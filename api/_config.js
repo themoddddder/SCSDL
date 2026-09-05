60202d9393b95f8057f5e0c6b18ee0198b9c2436
themoddddder <moddddder@gmail.com>
2026-09-05 17:08:00 -0400

    Change LIST2 value from 'Thingy' to 'Ignore'

--- api/_config.js
+++ api/_config.js
@@ -3,5 +3,5 @@
 // first list which the site defaults to upon visiting.
 export const LIST1 = 'SCSDL';
 // second list which the user has to switch over to in the top right once visiting
-export const LIST2 = 'Thingy';
+export const LIST2 = 'Ignore';
     
