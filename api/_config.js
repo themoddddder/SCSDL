317d25929b8566e8bbcdd0641ca2f6ac64fe4945
themoddddder <moddddder@gmail.com>
2026-09-05 14:55:14 -0400

    Update default list names in _config.js

--- api/_config.js
+++ api/_config.js
@@ -1,7 +1,7 @@
 // REPLACE THIS WITH YOUR LIST(s) NAMES ABBRIVIATED HERE AND IN public\js\config.js
 
 // first list which the site defaults to upon visiting.
-export const LIST1 = 'TPCL';
+export const LIST1 = 'SCSDL';
 // second list which the user has to switch over to in the top right once visiting
-export const LIST2 = 'TPL';
+export const LIST2 = 'Thingy';
     
