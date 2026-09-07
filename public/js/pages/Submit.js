2bfaf6be55f0dcf940c0af48be6ffeb1f5bd0435
themoddddder <moddddder@gmail.com>
2026-09-07 13:00:59 -0400

    Update Turnstile site key in Submit.js

--- public/js/pages/Submit.js
+++ public/js/pages/Submit.js
@@ -420,7 +420,7 @@ export default {
             this.turnstileToken = null;
             this.turnstileWidgetId = null;
 
-            const SITEKEY = '0x4AAAAAAClZM2Ug4vdAxfJI';
+            const SITEKEY = '0x4AAAAAAEql-DDXDSJmNZ46';
 
             this.$nextTick(() => {
                 const targetId = this.submissionType === 'record' ? '#turnstile-record' : '#turnstile-level';
